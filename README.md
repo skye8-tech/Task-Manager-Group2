42b3ed3208ff843ae66753c97f759d39e1cf74f3
asobosamuel <samuelasobo04@gmail.com>
2024-08-01 11:32:42 +0100

    project setup

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Task-Manager-Group2
 
-
-
-
 live link :https://roaring-moonbeam-1b7c5d.netlify.app/
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./assets/styles/*.css"],
+  content: ["./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
